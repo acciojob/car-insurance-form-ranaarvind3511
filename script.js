f134af7d544fe9311326ac07893bfdc94e9aa8f4
ranaarvind3511 <124048554+ranaarvind3511@users.noreply.github.com>
2023-07-14 22:16:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+const form = document.getElementById('registrationForm');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Retrieve form values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const carBrand = document.getElementById('cars').value;
+  const carAge = document.getElementById('carage').value;
+  const dateOfPurchase = document.getElementById('dop').value;
+  const password = document.getElementById('password').value;
+
+  // Perform further validation or processing
+  // ...
+});
